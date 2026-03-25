b1168abf85de2653bdfbb16af44847d4bd6e8dd3
digital2venture <digital2venture@gmail.com>
2026-03-26 02:08:38 +0800

    Add PointsTable.js

---
+++ PointsTable.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PointsTable = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Points</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>100</td>
+                    <td>Free Gas</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
+
+export default PointsTable;
